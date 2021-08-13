48c249b89f2d21b23ac1acb10f281faa71f93c9b
VictorHugo <victor.hugo@newpost.com>
2021-08-13 16:41:13 -0300

    campos virtuais

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,18 +9,19 @@ export interface UserInstance extends Model {
 
 export const User = sequelize.define<UserInstance>(
   "User",
-  {
+   {
     id: {
       primaryKey: true,
       autoIncrement: true,
       type: DataTypes.INTEGER,
     },
+
     name: {
       type: DataTypes.STRING,
-      // get() {
-      // quando for pegar os dados, passa por esse get
-      //   return this.getDataValue("name").toLowerCase();
-      // },
+      get() {
+        // quando for pegar os dados, passa por esse get
+        return this.getDataValue("name").toUpperCase();
+      },
       // set(value: string) {
 
       // Quando for salvar algo, passa por esse set
@@ -29,6 +30,15 @@ export const User = sequelize.define<UserInstance>(
       //   this.setDataValue("name", nameLow);
       // },
     },
+
+    firstLetterOfName: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        let name: string = this.getDataValue("name").toUpperCase();
+        return name.charAt(0);
+      },
+    },
+
     age: {
       type: DataTypes.INTEGER,
       defaultValue: 18,
